30379f56045ee6f874dc57e35bf9dac2485822b6
Rezende05 <gabriel.rezende008@gmail.com>
2023-05-18 20:14:15 -0300

    atualização do site

---
+++ js/script.js
@@ -0,0 +1,23 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import InitTooltip from "./modules/Tooltip.js";
+import initDropDownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumeros from "./modules/anima-numeros.js";
+import initFuncionamento from "./modules/funcionamento.js";
+
+
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+InitTooltip();
+initDropDownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
